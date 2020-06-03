45f4adcb21e86bae35c5eee703c9835a6d2fd039
Karina <karina-kucherenko@inbox.ru>
2020-06-03 17:11:34 +0300

    Files Init

---
+++ WebApplication MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication_MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "Это моё основное действие";
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            // return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            
+                ViewData["Message"] = "Hello " + name;
+                ViewData["NumTimes"] = numTimes;
+
+                return View();
+        }
+    }
+}
